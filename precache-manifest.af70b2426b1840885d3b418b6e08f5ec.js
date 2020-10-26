dba27f959120f97bf9f2659b3b639219a7e6db85
Arcenio <aquinoarcie@gmail.com>
2020-10-26 14:37:33 +0800

    Updates

---
+++ precache-manifest.af70b2426b1840885d3b418b6e08f5ec.js
@@ -0,0 +1,138 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00788113d57a7235ab44649b21773628",
+    "url": "/chess/index.html"
+  },
+  {
+    "revision": "f5b446b8be512366a053",
+    "url": "/chess/static/css/main.1e70146f.chunk.css"
+  },
+  {
+    "revision": "273619bd753cd68d2cf8",
+    "url": "/chess/static/js/2.5e2a8d46.chunk.js"
+  },
+  {
+    "revision": "4f00ef8916bc1113d1140153da13fbd7",
+    "url": "/chess/static/js/2.5e2a8d46.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f5b446b8be512366a053",
+    "url": "/chess/static/js/main.d7d5e521.chunk.js"
+  },
+  {
+    "revision": "fe54e1bce9cc9d1a3729",
+    "url": "/chess/static/js/runtime-main.bff3f106.js"
+  },
+  {
+    "revision": "aeda322e8c51b7015cab960bde6e19fe",
+    "url": "/chess/static/media/bishop.aeda322e.svg"
+  },
+  {
+    "revision": "e26586ce4265395b817de7c5420a50c8",
+    "url": "/chess/static/media/bishop.e26586ce.svg"
+  },
+  {
+    "revision": "f7c0a6a0d118cd4356f7641da650d574",
+    "url": "/chess/static/media/caution.f7c0a6a0.svg"
+  },
+  {
+    "revision": "8570fd06b03535323f3511cc73d6333c",
+    "url": "/chess/static/media/chat-blades.8570fd06.svg"
+  },
+  {
+    "revision": "cbbe33322fd2a8a9e174268010cf2cb8",
+    "url": "/chess/static/media/chat-move.cbbe3332.svg"
+  },
+  {
+    "revision": "7dabc4c2ca255a5a504323478aa02b75",
+    "url": "/chess/static/media/chat-start.7dabc4c2.svg"
+  },
+  {
+    "revision": "a32f45ff366138328e16e89ef4c9032c",
+    "url": "/chess/static/media/chess-broken.a32f45ff.svg"
+  },
+  {
+    "revision": "d359bdb7c9581830fc34d53b88f0c95a",
+    "url": "/chess/static/media/chess-castling.d359bdb7.svg"
+  },
+  {
+    "revision": "e5930f6f6ef74b3c49ffd70e57197c9f",
+    "url": "/chess/static/media/chess-lose.e5930f6f.svg"
+  },
+  {
+    "revision": "d56b23e98e8436fd02ad7d2c3c0b6f5c",
+    "url": "/chess/static/media/chess-move.d56b23e9.svg"
+  },
+  {
+    "revision": "0a16a80bda5a8d02e4468d027e0f3d34",
+    "url": "/chess/static/media/chess-searching.0a16a80b.svg"
+  },
+  {
+    "revision": "1cb14f03bfc8800ac43be8c602aff382",
+    "url": "/chess/static/media/chess-win.1cb14f03.svg"
+  },
+  {
+    "revision": "12567e3b260d1d99853283374bf33154",
+    "url": "/chess/static/media/copy.12567e3b.svg"
+  },
+  {
+    "revision": "b2138751ec3c40bb5c24fcea41aaff9d",
+    "url": "/chess/static/media/error.b2138751.svg"
+  },
+  {
+    "revision": "628e293d3a8492e20c20640a8b2e829d",
+    "url": "/chess/static/media/flag.628e293d.svg"
+  },
+  {
+    "revision": "0f5c85654c8a1ba4cf89ef9988301484",
+    "url": "/chess/static/media/king.0f5c8565.svg"
+  },
+  {
+    "revision": "6e80e1137d4aa36059b7054d6448c406",
+    "url": "/chess/static/media/king.6e80e113.svg"
+  },
+  {
+    "revision": "18b1495c47a0685a231fcd32fb1e16c7",
+    "url": "/chess/static/media/knight.18b1495c.svg"
+  },
+  {
+    "revision": "87aa327bbe75bfc61d6bae896643704f",
+    "url": "/chess/static/media/knight.87aa327b.svg"
+  },
+  {
+    "revision": "0150c595bc208284e2fdfb29929ddf7c",
+    "url": "/chess/static/media/pawn.0150c595.svg"
+  },
+  {
+    "revision": "3af36ba9f2c2f637df797a84686d5e60",
+    "url": "/chess/static/media/pawn.3af36ba9.svg"
+  },
+  {
+    "revision": "7cf8de5a1c80e61ceffafd335f71879d",
+    "url": "/chess/static/media/queen.7cf8de5a.svg"
+  },
+  {
+    "revision": "e91fa9566b5258402383d7f5e8831c05",
+    "url": "/chess/static/media/queen.e91fa956.svg"
+  },
+  {
+    "revision": "b97028e8438362e3dc93e6f1105e7885",
+    "url": "/chess/static/media/reload.b97028e8.svg"
+  },
+  {
+    "revision": "62d0583ca47ed46909f59ecf8e95c672",
+    "url": "/chess/static/media/rook.62d0583c.svg"
+  },
+  {
+    "revision": "859b69626dc515721bbefdb56e13a8a3",
+    "url": "/chess/static/media/rook.859b6962.svg"
+  },
+  {
+    "revision": "493bde11d92e92ac2a216fc7dd0d13c5",
+    "url": "/chess/static/media/send.493bde11.svg"
+  },
+  {
+    "revision": "ce6e2233d12b5a2fa2b7bd168acb14dd",
+    "url": "/chess/static/media/settings.ce6e2233.svg"
+  }
+]);
